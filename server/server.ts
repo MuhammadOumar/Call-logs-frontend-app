a73c5bd668779580def57ed9d7cb10440d328b12
Muhammad Oumar <icloud82k1@gmail.com>
2025-06-14 21:38:54 +0500

    Fix Express type definitions

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
 });
 
 app.listen(port, () => {
