789d27899bcf8b6ec0dd7cef2a90f59d1f7accf3
Muhammad Oumar <icloud82k1@gmail.com>
2025-06-14 21:18:47 +0500

    Fix Express type definitions

--- server/server.ts
+++ server/server.ts
@@ -16,10 +16,10 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
 });
 
 app.listen(port, () => {
